26cda77daa658c36794a4576c73cfc5be21e3dd3
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-10-27 22:32:27 +0530

    added for_each loops and conditions

--- instance.tf
+++ instance.tf
@@ -1,69 +1,21 @@
 # jenkins server setup
-resource "aws_instance" "jenkins" {
-  ami               = var.AMIS["ubuntu18"]
-  instance_type     = "t2.small"
-  count             = 1
-  availability_zone = "us-east-1a"
-  key_name          = "vprofile-prod-key"
-  vpc_security_group_ids = [
-    "sg-0eb600aa6ac4e7b1e"
-  ]
-  user_data = file("./userdata/jenkins-setup.sh")
-  tags = {
-    Name = "Jenkins-server"
-  }
-}
-
 resource "aws_instance" "vms-servers" {
-  for_each = toset(var.VMS)
-  name = each.value
-  ami               = var.AMIS[name]
-  instance_type     = name == jenkins ? "t2.small" : "t2.medium"
-  count             = 1
+  for_each          = toset(var.VMS)
+  ami               = var.AMIS[each.key]
+  instance_type     = each.key != "jenkins" ? "t2.medium": "t2.small"
   availability_zone = "us-east-1a"
   key_name          = "vprofile-prod-key"
   vpc_security_group_ids = [
-    "${var.SG[name]}"
+    "${var.SG[each.key]}"
   ]
-  user_data = file("./userdata/${name}-setup.sh")
+  user_data = file("./userdata/${each.key}-setup.sh")
   tags = {
-    Name = "${name}-server"
-  }
-}
-
-# nexus server setup
-resource "aws_instance" "nexus" {
-  ami               = var.AMIS["centos"]
-  instance_type     = "t2.medium"
-  count             = 1
-  availability_zone = "us-east-1a"
-  key_name          = "vprofile-prod-key"
-  vpc_security_group_ids = [
-    "sg-0917059ab4119fad2"
-  ]
-  user_data = file("./userdata/nexus-setup.sh")
-  tags = {
-    Name = "nexus-server"
-  }
-}
-
-# sonar server setup
-resource "aws_instance" "sonar" {
-  ami               = var.AMIS["ubuntu"]
-  instance_type     = "t2.medium"
-  count             = 1
-  availability_zone = "us-east-1a"
-  key_name          = "vprofile-prod-key"
-  vpc_security_group_ids = [
-    "sg-0e3d6a4e659198775"
-  ]
-  user_data = file("./userdata/sonar-setup.sh")
-  tags = {
-    Name = "sonar-server"
+    Name = "${each.key}-server"
   }
 }
 
 output "PublicIP" {
-  value       = "jenkins ip: ${aws_instance.jenkins[0].public_ip} sonar ip: ${aws_instance.sonar[0].public_ip}, nexus ip: ${aws_instance.nexus[0].public_ip}"
+  for_eah = toset(var.VMS)
+  value       = "${each.key} ip: ${aws_instance.vms-servers[each.key].public_ip}"
   description = "PublicIP address details"
 }
--- vars.tf
+++ vars.tf
@@ -3,24 +3,24 @@ variable "REGION" {
 }
 
 variable "AMIS" {
-  type = map
+  type = map(any)
   default = {
     jenkins = "ami-0ee23bfc74a881de5"
-    sonar = "ami-0149b2da6ceec4bb0"
-    nexus = "ami-09d3b3274b6c5d4aa"
+    sonar   = "ami-0149b2da6ceec4bb0"
+    nexus   = "ami-09d3b3274b6c5d4aa"
   }
 }
 
 variable "SG" {
-  type = map
+  type = map(any)
   default = {
     jenkins = "sg-0eb600aa6ac4e7b1e"
-    nexus = "sg-0917059ab4119fad2"
-    sonar = "sg-0e3d6a4e659198775"
+    nexus   = "sg-0917059ab4119fad2"
+    sonar   = "sg-0e3d6a4e659198775"
   }
 }
 
 variable "VMS" {
-  type = list
+  type    = list(any)
   default = ["jenkins", "nexus", "sonar"]
 }
