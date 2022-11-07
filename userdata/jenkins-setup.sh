b1998a79231ff5cf5235b5532eb29bf8e1ff3e83
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-11-07 14:42:26 +0530

    updated nginx conf for reverse proxy

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -15,11 +15,8 @@ sudo apt-get install jenkins -y
 apt-get install nginx -y
 rm -rf /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/sites-available/default
+
 cat <<EOT> /etc/nginx/sites-available/jenkins
-proxy_cache_path  /tmp/nginx_cache levels=1:2 keys_zone=my-cache:10m max_size=1000m inactive=600m;
-proxy_temp_path /tmp/cache/tmp; 
-
-
 upstream jenkins {
   server localhost:8080;
 }
@@ -31,14 +28,6 @@ server{
 
     location / {
       proxy_pass http://jenkins;
-      proxy_cache my-cache;
-      proxy_set_header Host \$host;
-      proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-      proxy_ignore_headers Set-Cookie; 
-      proxy_ignore_headers Cache-Control; 
-      proxy_cache_bypass \$http_secret_header;
-      add_header X-Cache-Status \$upstream_cache_status;
     }
 }
 EOT
--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -38,10 +38,6 @@ systemctl enable nexus
 amazon-linux-extras install nginx1 -y
 rm -rf /etc/nginx/conf.d/default.conf
 cat <<EOT> /etc/nginx/conf.d/default.conf
-proxy_cache_path  /tmp/nginx_cache levels=1:2 keys_zone=my-cache:10m max_size=1000m inactive=600m;
-proxy_temp_path /tmp/cache/tmp; 
-
-
 upstream nexus {
   server localhost:8081;
 }
@@ -53,14 +49,6 @@ server{
 
     location / {
       proxy_pass http://nexus;
-      proxy_cache my-cache;
-      proxy_set_header Host \$host;
-      proxy_set_header X-Real-IP \$remote_addr;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-      proxy_ignore_headers Set-Cookie; 
-      proxy_ignore_headers Cache-Control; 
-      proxy_cache_bypass \$http_secret_header;
-      add_header X-Cache-Status \$upstream_cache_status;
     }
 }
 EOT
