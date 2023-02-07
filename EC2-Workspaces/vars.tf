3f7d80ba372150bd7da91c21eec48d9f016621de
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-02-07 17:48:32 +0530

    created specific ami id for specific workspace

--- EC2-Workspaces/vars.tf
+++ EC2-Workspaces/vars.tf
@@ -1,4 +1,14 @@
 # creating a local variable
 locals {
     env = "${terraform.workspace}"  # geting a current workspace
+
+    # creating a dynamic ami id based on the workspace
+    amiid_env = {
+        default = "amiid_default"
+        staging = "amiid_staging"
+        production = "amiid_production"
+    }
+
+    # lookup command to search for specific map in local
+    amiid = "${lookup(local.amiid_env, local.env)}"
 }
